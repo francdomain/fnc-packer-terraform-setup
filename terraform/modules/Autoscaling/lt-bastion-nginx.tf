c6c7707e889bc531147aa55323eb9151dac2e844
francdomain <cfrancmail@gmail.com>
2024-08-16 12:09:30 +0100

    edited bastion lt

--- terraform/modules/Autoscaling/lt-bastion-nginx.tf
+++ terraform/modules/Autoscaling/lt-bastion-nginx.tf
@@ -2,7 +2,7 @@
 
 resource "aws_launch_template" "bastion-launch-template" {
   image_id               = var.ami-bastion
-  instance_type          = "t2.small"
+  instance_type          = "t3.small"
   vpc_security_group_ids = var.bastion-sg
 
   iam_instance_profile {
